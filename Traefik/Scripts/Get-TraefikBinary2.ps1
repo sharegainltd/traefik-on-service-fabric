5da74985d26a2be8e5240c7ef29df2ae18173d28
idany@sela.co.il <idany@sela.co.il>
2018-03-27 19:41:55 +0300

    fix watchdog download to create directory if missing

--- Traefik/Scripts/Get-TraefikBinary2.ps1
+++ Traefik/Scripts/Get-TraefikBinary2.ps1
@@ -1,6 +1,8 @@
 ﻿param($version='v1.5.4',$watchversion='v0.0.3')
 #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+
+New-Item -ItemType Directory -Force -Path $PSScriptRoot+"/../ApplicationPackageRoot/TraefikPkg/Code" 
 $releases=Invoke-RestMethod -Uri https://api.github.com/repos/containous/traefik/releases -Method Get -ContentType 'application/json'
 $release=$releases | ?{$_.tag_name -eq $version}
 $asset=$release.assets | ?{$_.name -eq 'traefik_windows-amd64.exe'}
@@ -8,6 +10,7 @@ $url=$asset.browser_download_url
 $outfile = $PSScriptRoot+"/../ApplicationPackageRoot/TraefikPkg/Code/traefik.exe"
 Invoke-WebRequest -Uri $url -OutFile $outfile -UseBasicParsing
 
+New-Item -ItemType Directory -Force -Path $PSScriptRoot+"/../ApplicationPackageRoot/Watchdog/Code"
 $watchreleases=Invoke-RestMethod -Uri https://api.github.com/repos/lawrencegripper/traefik-appinsights-watchdog/releases -Method Get -ContentType 'application/json'
 $watchrelease=$watchreleases | ?{$_.tag_name -eq $watchversion}
 $watchasset=$watchrelease.assets | ?{$_.name -eq 'windows_traefik-appinsights-watchdog.exe'}
