36f7ab312bbd14b644970fac8f873dc6d14f90c1
idany@sela.co.il <idany@sela.co.il>
2018-03-27 19:30:50 +0300

    updated binary download to include watchdog

--- Traefik/Scripts/Get-TraefikBinary2.ps1
+++ Traefik/Scripts/Get-TraefikBinary2.ps1
@@ -1,4 +1,4 @@
-﻿param($version='v1.5.4')
+﻿param($version='v1.5.4',$watchversion='v0.0.3')
 #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 $releases=Invoke-RestMethod -Uri https://api.github.com/repos/containous/traefik/releases -Method Get -ContentType 'application/json'
@@ -7,4 +7,12 @@ $asset=$release.assets | ?{$_.name -eq 'traefik_windows-amd64.exe'}
 $url=$asset.browser_download_url
 $outfile = $PSScriptRoot+"/../ApplicationPackageRoot/TraefikPkg/Code/traefik.exe"
 Invoke-WebRequest -Uri $url -OutFile $outfile -UseBasicParsing
+
+$watchreleases=Invoke-RestMethod -Uri https://api.github.com/repos/lawrencegripper/traefik-appinsights-watchdog/releases -Method Get -ContentType 'application/json'
+$watchrelease=$watchreleases | ?{$_.tag_name -eq $watchversion}
+$watchasset=$watchrelease.assets | ?{$_.name -eq 'windows_traefik-appinsights-watchdog.exe'}
+$watchurl=$watchasset.browser_download_url
+$watchoutfile = $PSScriptRoot+"/../ApplicationPackageRoot/Watchdog/Code/traefik-appinsights-watchdog.exe"
+Invoke-WebRequest -Uri $watchurl -OutFile $watchoutfile -UseBasicParsing
+
 & $outfile version
